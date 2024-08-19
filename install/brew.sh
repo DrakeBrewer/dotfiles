b5ab1689342c98d8176bd7558e0ece83d4a7ab30
Drake Brewer <drakeb@rightline.com>
2024-08-18 20:00:14 -0700

    fix: install scripts

--- install/brew.sh
+++ install/brew.sh
@@ -1,8 +1,15 @@
 echo "Installing Homebrew..."
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew update
-brew upgrade
+# install homebrew if not already installed
+if test ! $(which brew); then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+
+	brew update
+	brew upgrade
+fi
 
 echo "Homebrew installed!"
 
@@ -10,6 +17,7 @@ sleep 1
 
 echo "Installing formulae and casks..."
 
+brew tap homebrew/bundle
 brew bundle install --file "~/.dotfiles/install/Brewfile"
 
 echo "Formulae and casks installed!"
