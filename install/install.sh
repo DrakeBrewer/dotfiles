90a78f731aeb9ad3b03698829c4a8470cd9effdf
Drake Brewer <drakeb@rightline.com>
2024-08-18 21:06:40 -0700

    fix: removed omz install as it was causing issues put omz specific settings in a conditional

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,6 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
-# install oh my zsh
-if test ! $(which omz); then
-	/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-fi
-
 # Install brew stuff.
 . "$DOTFILES_DIR/install/brew.sh"
 
