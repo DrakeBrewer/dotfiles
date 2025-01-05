f6f8d97dc7e3aa77c8407dbf3d64a256d46c673e
drakebrewer <drake@netstorms.com>
2025-01-05 11:19:53 -0800

    feat: added harpoon to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -325,7 +325,6 @@ require('lazy').setup({
               -- end,
               -- cwd = '${workspaceFolder}',
               -- stopOnEntry = false,
-              args = { '-5', './datasets/mobydick.txt' },
             },
           }
         end,
@@ -452,6 +451,51 @@ require('lazy').setup({
           { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
         },
       }
+    end,
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+
+      local telescope_conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = telescope_conf.file_previewer {},
+            sorter = telescope_conf.generic_sorter {},
+          })
+          :find()
+      end
+
+      vim.keymap.set('n', '<leader>hl', function()
+        require 'harpoon'
+        toggle_telescope(harpoon:list())
+      end, { desc = '[H]arpoon [L]ist' })
+
+      vim.keymap.set('n', '<leader>ha', function()
+        harpoon:list():add()
+      end, { desc = '[H]arpoon [A]dd' })
+
+      vim.keymap.set('n', '<leader>hd', function()
+        harpoon:list():remove()
+      end, { desc = '[H]arpoon [D]elete' })
+
+      vim.keymap.set('n', '<leader>hc', function()
+        harpoon:list():clear()
+      end, { desc = '[H]arpoon [C]lear' })
     end,
   },
 
