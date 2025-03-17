b3f42db7fa3c5297e70830b081bc1db2cc815ae5
drakebrewer <drake@netstorms.com>
2025-03-17 13:35:36 -0500

    feat: cleaned up nvim config structure * fix: removed keybind * feat: migrated plugins to separate files * feat: add debugger for go

---
+++ nvim/lua/plugins/debugger.lua
@@ -0,0 +1,106 @@
+return {
+  'rcarriga/nvim-dap-ui',
+  dependencies = {
+    {
+      'mfussenegger/nvim-dap',
+      name = 'nvim-dap',
+      dir = os.getenv 'HOME' .. '/.local/share/nvim/lazy/nvim-dap',
+      dependencies = {
+        { 'leoluz/nvim-dap-go', name = 'nvim-dap-go', dir = os.getenv 'HOME' .. '/.local/share/nvim/lazy/dap-go' },
+      },
+      keys = {
+        {
+          '<leader>dl',
+          function()
+            require('dap').run_last()
+          end,
+          desc = '[l] dap run last',
+        },
+        {
+          '<leader>dc',
+          function()
+            require('dap').continue()
+          end,
+          desc = '[C]ontinue',
+        },
+        {
+          '<leader>db',
+          function()
+            require('dap').set_breakpoint()
+          end,
+          desc = '[b] set breakpoint',
+        },
+        {
+          '<leader>dw',
+          function()
+            require('dapui').elements.watches.add '/nat ... variable'
+          end,
+          desc = '[W]atch',
+        },
+        {
+          '<leader>dt',
+          function()
+            require('dap').terminate()
+          end,
+          desc = '[T]erminate',
+        },
+      },
+      config = function()
+        local dap = require 'dap'
+        dap.adapters.codelldb = {
+          type = 'server',
+          port = '${port}',
+          executable = {
+            command = os.getenv 'HOME' .. '/.local/share/nvim/mason/bin/codelldb',
+            args = {
+              '--port',
+              '${port}',
+            },
+          },
+        }
+        dap.adapters.dlv = {
+          type = 'server',
+          port = '${port}',
+          executable = {
+            command = os.getenv 'HOME' .. '/.local/share/nvim/mason/bin/dlv',
+            args = {
+              'dap',
+              '-l',
+              '127.0.0.1' .. ':' .. '${port}',
+            },
+          },
+        }
+        dap.configurations.c = {
+          {
+            type = 'codelldb',
+            request = 'launch',
+            name = 'lldb',
+            program = '${command:pickFile}',
+          },
+        }
+        dap.configurations.go = {
+          {
+            type = 'dlv',
+            request = 'launch',
+            name = 'dlv',
+            program = '${file}',
+          },
+        }
+      end,
+    },
+    'nvim-neotest/nvim-nio',
+  },
+  keys = {
+    {
+      '<leader>dui',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = '[dui] toggle dapui',
+    },
+  },
+  opts = {},
+  config = function(_, opts)
+    require('dapui').setup(opts)
+  end,
+}
---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,29 @@
+return {
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+    },
+    config = true,
+    cmd = 'Neogit',
+    keys = {
+      {
+        '<leader>gs',
+        '<cmd>Neogit<CR>',
+      },
+    },
+  },
+}
